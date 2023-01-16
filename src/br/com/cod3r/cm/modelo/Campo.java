f6cefc13364f8ac37ad42cdaba8aaefda8e38953
Mateus Campos Peverari <mateus.peverari@gmail.com>
2023-01-16 11:27:11 -0300

    feat: added the 'adicionarVizinho' method to the 'Campo.java' class and it's tests

--- src/br/com/cod3r/cm/modelo/Campo.java
+++ src/br/com/cod3r/cm/modelo/Campo.java
@@ -17,4 +17,24 @@ public class Campo {
         this.coluna = coluna;
     }
 
+    boolean adicionarVizinho(Campo vizinho) {
+        boolean linhaDiferente = this.linha != vizinho.linha;
+        boolean colunaDiferente = this.coluna != vizinho.coluna;
+        boolean diagonal = colunaDiferente && linhaDiferente;
+
+        int deltaLinha = Math.abs(linha - vizinho.linha);
+        int deltaColuna = Math.abs(coluna - vizinho.coluna);
+        int deltaGeral = deltaColuna + deltaLinha;
+
+        if (deltaGeral == 1 && !diagonal) {
+            vizinhos.add(vizinho);
+            return true;
+        } else if(deltaGeral == 2 && diagonal) {
+            vizinhos.add(vizinho);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
