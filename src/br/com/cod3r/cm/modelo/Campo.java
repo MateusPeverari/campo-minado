a68f22fb24ea0b92c41d666671f3d1c24379c80f
Mateus Campos Peverari <mateus.peverari@gmail.com>
2023-01-16 11:41:23 -0300

    feat: adding the 'abrir()/alternarMarcacao()/vizinhacaSegura()' methods to the 'Campo.java' class

--- src/br/com/cod3r/cm/modelo/Campo.java
+++ src/br/com/cod3r/cm/modelo/Campo.java
@@ -1,5 +1,7 @@
 package br.com.cod3r.cm.modelo;
 
+import br.com.cod3r.cm.excecao.ExplosaoException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,6 +37,33 @@ public class Campo {
         } else {
             return false;
         }
+    }
+
+    void alternarMarcacao(){
+        if (!aberto) {
+            marcado = !marcado;
+        }
+    }
+
+    boolean abrir() {
+        if (!aberto && !marcado) {
+            aberto = true;
+
+            if (minado) {
+                throw new ExplosaoException();
+            }
+
+            if (vizinhancaSegura()) {
+                vizinhos.forEach(v -> v.abrir());
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean vizinhancaSegura() {
+        return vizinhos.stream().noneMatch(v -> v.minado);
     }
 
 }
