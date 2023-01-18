2ef89f66cc1e41bc3984275cd67bd3ec24afb1f8
Mateus Campos Peverari <mateus.peverari@gmail.com>
2023-01-18 11:08:17 -0300

    feat: implementing 'toString' method on 'Tabuleiro.java'. Creating 'Aplicacao.java' class

--- src/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -22,6 +22,20 @@ public class Tabuleiro {
         sortearMinas();
     }
 
+    public void abrir(int linha, int coluna) {
+        campos.parallelStream()
+                .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                .findFirst()
+                .ifPresent(c -> c.abrir());
+    }
+
+    public void alternarMarcacao(int linha, int coluna) {
+        campos.parallelStream()
+                .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                .findFirst()
+                .ifPresent(c -> c.alternarMarcacao());
+    }
+
     private void gerarCampos() {
         for (int linha = 0; linha < linhas; linha++) {
             for (int coluna = 0; coluna < colunas; coluna++) {
@@ -59,5 +73,22 @@ public class Tabuleiro {
         sortearMinas();
     }
 
-    
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        int i = 0;
+        for (int l = 0; l < linhas; l++) {
+
+            for (int c = 0; c < linhas; c++) {
+                sb.append(" ");
+                sb.append(campos.get(i));
+                sb.append(" ");
+                i++;
+            }
+            sb.append("\n");
+
+        }
+        return sb.toString();
+    }
+
 }
